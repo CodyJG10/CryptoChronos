24b5745f47509644d220f8502605598bc51dbe8e
Cody Gordon <codyjg10@gmail.com>
2022-01-25 23:36:12 -0500

    updates

--- NFT.ContractInteraction/NFT.ContractInteraction.Server/Interfaces/INftController.cs
+++ NFT.ContractInteraction/NFT.ContractInteraction.Server/Interfaces/INftController.cs
@@ -4,7 +4,7 @@ namespace NFT.ContractInteraction.Server.Interfaces
 {
     public interface INftController
     {
-        public Task<MintWatchReceipt> MintNft(MintWatchModel model);
+        //public Task<MintWatchReceipt> MintNft(MintWatchModel model);
         public Task<List<string>> GetAllNfts(string ownerAddress);
         public Task<List<string>> GetAllNftsFromWeb3();
         public Task<string> GetTokenUri(string tokenId);
