4708c5ac62215a05e5812cbb1798db6767037227
Cody Gordon <codyjg10@gmail.com>
2022-03-01 03:01:20 -0500

    Initial

---
+++ CryptoChronos/Client/wwwroot/theme-assets/js/nav_menu.js
@@ -0,0 +1,15 @@
+﻿// menu popup
+function initNavMenu() {
+	let header = $('.js-header');
+	let popup = header.find('.menu__popup');
+	let icon = header.find('.has_popup');
+
+	icon.on('click', function (e) {
+		e.stopPropagation();
+		popup.toggleClass('visible');
+	});
+	$(document).on('click', 'body', function (e) {
+		if (!$(e.target).is('.visible'))
+			popup.removeClass('visible');
+	})
+}
---
+++ CryptoChronos/Server/Migrations/20220227075912_RemovedSelling.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatchNFT.Server.Migrations
+{
+    public partial class RemovedSelling : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuctionMetadata");
+
+            migrationBuilder.DropTable(
+                name: "LocalListingRecords");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuctionMetadata",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AuctionAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Views = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuctionMetadata", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LocalListingRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageCID = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    ListingAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListingType = table.Column<int>(type: "int", nullable: false),
+                    SellerAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TokenId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalListingRecords", x => x.Id);
+                });
+        }
+    }
+}
